43a028d4ae903a570e0e54c4292613f8087e619d
iigmir <roc120j@gmail.com>
2020-07-30 23:11:59 +0800

    Give up the move out idea due to CoC

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import Vue from "vue";
-import Vuex from "vuex";
 import App from "./App.vue";
 import router from "./router";
-import vuex_store from "./vuex/index.js";
+import store from "./store/index.js";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  store: new Vuex.Store(vuex_store),
+  store,
   render: h => h(App)
 }).$mount("#app");
