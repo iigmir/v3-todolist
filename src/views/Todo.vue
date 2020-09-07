d435ceb7a4abab0bc1a86f01255b1e4a012a1f7c
iigmir <roc120j@gmail.com>
2020-09-08 01:44:00 +0800

    rhd

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -11,12 +11,9 @@
         <div>
             <form v-on:submit="add_todo">
                 <fieldset>
-                    <legend>Write your first todo</legend>
+                    <legend>Write your todos</legend>
                     <label for="todo1">Todo 1</label>
                     <input type="text" id="todo1" name="todo1" />
-                </fieldset>
-                <fieldset>
-                    <legend>Write your second todo</legend>
                     <label for="todo2">Todo 2</label>
                     <input type="text" id="todo2" name="todo2" />
                 </fieldset>
