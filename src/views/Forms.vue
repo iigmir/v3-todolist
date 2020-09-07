179cd484c2f4b80e116c369ed582a254b746475d
iigmir <roc120j@gmail.com>
2020-09-08 01:46:56 +0800

    Sp

---
+++ src/views/Forms.vue
@@ -0,0 +1,15 @@
+<template>
+    <form v-on:submit="add_todo">
+        <fieldset>
+            <legend>Write your todos</legend>
+            <label for="todo1">Todo 1</label>
+            <input type="text" id="todo1" name="todo1" />
+        </fieldset>
+        <fieldset>
+            <legend>Write your second todo</legend>
+            <label for="todo2">Todo 2</label>
+            <input type="text" id="todo2" name="todo2" />
+        </fieldset>
+        <button type="submit">Submit todo</button>
+    </form>
+</template>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -9,25 +9,20 @@
             </ul>
         </div>
         <div>
-            <form v-on:submit="add_todo">
-                <fieldset>
-                    <legend>Write your todos</legend>
-                    <label for="todo1">Todo 1</label>
-                    <input type="text" id="todo1" name="todo1" />
-                    <label for="todo2">Todo 2</label>
-                    <input type="text" id="todo2" name="todo2" />
-                </fieldset>
-                <button type="submit">Submit todo</button>
-            </form>
+            <Forms />
         </div>
     </main>
 </template>
 
 <script>
 import { ref, reactive } from "vue";
+import Forms from "./Forms.vue";
 
 export default {
     name: "Todo",
+    components: {
+        Forms
+    },
     setup()
     {
         const readersNumber = ref(0);
