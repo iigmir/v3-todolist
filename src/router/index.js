30c54bdeac690f695b3b404bc801633db7f0888e
iigmir <roc120j@gmail.com>
2020-09-09 13:09:03 +0800

    Delete route

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,12 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import Main from "../views/Main.vue";
 import Todo from "../views/Todo/index.vue";
 
 const routes = [
     {
         path: "/",
-        name: "Main",
-        component: Main
-    },
-    {
-        path: "/todo",
         name: "Todo",
         component: Todo
-    },
+    }
 ];
 
 const router = createRouter({
